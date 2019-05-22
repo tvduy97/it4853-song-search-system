387bce40d5fe12ccf0b7ef9b1e20a3b070fbadd9
Tran Van Duy <tranduy200997@gmail.com>
2019-05-23 01:08:48 +0700

    basic search

--- resources/views/basic_result.blade.php
+++ resources/views/basic_result.blade.php
@@ -3,21 +3,16 @@
 <div class="container">
       <div class="row">
         <div class="s01-result" style="width: 100%;">
-          <form>
+          <form method="get" action="{{ route('basic.search') }}">
             <div class="inner-form">
               <div class="input-field first-wrap" style="margin-right: -9%;">
-                <input class="form-control" type="text" placeholder="Bạn muốn tìm gì ?">
+                @include('components.basic_input')
               </div>
               <div class="input-field second-wrap" style="margin-right: -10%;">
-                <select>
-                  <option style="border-radius: 25px;">Tất Cả</option>
-                  <option>Tên Bài Hát</option>
-                  <option>Ca Sĩ</option>
-                  <option>Lời Bài Hát</option>
-                </select>
+                @include('components.basic_filter')
               </div>
               <div class="input-field third-wrap">
-                <button class="btn-search" type="button">Tìm Kiếm</button>
+                @include('components.basic_btn_search')
               </div>
             </div>
           </form>
@@ -25,63 +20,37 @@
       </div>
       <div class="row">
         <div class="container">
-          <h4>Kết Quả Tìm Kiếm: (0.001s)</h4>
+          @if (count($resultset) > 0)
+          <h4>Tìm thấy {{ $numFound}} kết quả trong {{ $queryTime }} s</h4>
+            @if ($totalPage > 1)
+              <h3>Trang {{ $currentPage }}</h3>
+            @endif
           <hr>
           <ul class="list-group">
-            <li class="list-group-item">
-              <ul class="list-group">
-                <h4 class="fix-margin-bottom">Bài Hát:</h4>
-                <p class="fix-margin-bottom">Ca Sĩ:</p>
-                <p class="fix-margin-bottom">Lời Bài Hát:</p>
-              </ul>
-            </li>
-            <li class="list-group-item">
-              <ul class="list-group">
-                <h4 class="fix-margin-bottom">Bài Hát:</h4>
-                <p class="fix-margin-bottom">Ca Sĩ:</p>
-                <p class="fix-margin-bottom">Lời Bài Hát:</p>
-              </ul>
-            </li>
-            <li class="list-group-item">
-              <ul class="list-group">
-                <h4 class="fix-margin-bottom">Bài Hát:</h4>
-                <p class="fix-margin-bottom">Ca Sĩ:</p>
-                <p class="fix-margin-bottom">Lời Bài Hát:</p>
-              </ul>
-            </li>
-            <li class="list-group-item">
-              <ul class="list-group">
-                <h4 class="fix-margin-bottom">Bài Hát:</h4>
-                <p class="fix-margin-bottom">Ca Sĩ:</p>
-                <p class="fix-margin-bottom">Lời Bài Hát:</p>
-              </ul>
-            </li>
-            <li class="list-group-item">
-              <ul class="list-group">
-                <h4 class="fix-margin-bottom">Bài Hát:</h4>
-                <p class="fix-margin-bottom">Ca Sĩ:</p>
-                <p class="fix-margin-bottom">Lời Bài Hát:</p>
-              </ul>
-            </li>
-            <li class="list-group-item">
-              <ul class="list-group">
-                <h4 class="fix-margin-bottom">Bài Hát:</h4>
-                <p class="fix-margin-bottom">Ca Sĩ:</p>
-                <p class="fix-margin-bottom">Lời Bài Hát:</p>
-              </ul>
-            </li>
+            @foreach ($resultset as $data)
+              <li class="list-group-item">
+                <ul class="list-group">
+                  <a href="{{ $data->link[0] }}" target="blank" tool>
+                    <h4 class="fix-margin-bottom">Bài Hát: {{ $data->title[0] }}</h4>
+                  </a>
+                  <p class="fix-margin-bottom">Ca Sĩ: {{ $data->singer[0] }}</p>
+                  <p class="fix-margin-bottom">Lời Bài Hát:
+                    @if (isset($data->lyric))
+                      {{$data->lyric[0]}}
+                    @else
+                      Chưa có lời bài hát
+                    @endif
+                  </p>
+                </ul>
+              </li>
+            @endforeach
           </ul>
+          @else
+            Không tìm thấy bài hát nào.
+          @endif
         </div> 
       </div>
       <br>
-      <div class="row justify-content-center">
-        <ul class="pagination">
-          <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-          <li class="page-item"><a class="page-link" href="#">1</a></li>
-          <li class="page-item"><a class="page-link" href="#">2</a></li>
-          <li class="page-item"><a class="page-link" href="#">3</a></li>
-          <li class="page-item"><a class="page-link" href="#">Next</a></li>
-        </ul>
-      </div>
+      @include('components.paginate')
     </div>
 @endsection
